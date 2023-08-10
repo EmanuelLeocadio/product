c444706579db82360d3fbef494547cc3913dade6
EmanuelLeocadio <emanuelleocadioramalho@gmail.com>
2023-08-09 21:22:48 -0300

    first commit

---
+++ src/main/java/br/com/tech4me/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
